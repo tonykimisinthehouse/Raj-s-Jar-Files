17a240de008ca869a00a5a8031170ba7f66a8371
Dong-Hyun (Tony) Kim <tonykim@gatech.edu>
2019-02-15 13:32:54 -0500

    You can now run the app in Android Studio

---
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.gatech.cs2340.rajsjarfiles.spacetrader;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
