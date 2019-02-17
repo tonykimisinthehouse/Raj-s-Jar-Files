85d383615a0caa0397accf2ac258fe827f7d89d9
mljustinli <ml.justin.li@gmail.com>
2019-02-17 11:55:49 -0500

    Fix the merge with Tony's configuration activity edits.

--- android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/MainActivity.java
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/MainActivity.java
@@ -6,22 +6,57 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Arrays;
+
+import edu.gatech.cs2340.rajsjarfiles.spacetrader.entity.GameDifficulty;
+import edu.gatech.cs2340.rajsjarfiles.spacetrader.entity.Player;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editPlayerName;
+    private EditText editPilotPoints;
+    private EditText editFighterPoints;
+    private EditText editTraderPoints;
+    private EditText editEngineerPoints;
+    private Spinner difficultySpinner;
+
+    /* **********
+        Data for player being edited.
+     */
+    private Player player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        /*
+         * Grab the dialog widgets so we can get info for later
+         */
+        editPlayerName = findViewById(R.id.player_name_input);
+        editPilotPoints = findViewById(R.id.pilot_skill_input);
+        editFighterPoints = findViewById(R.id.fighter_skill_input);
+        editTraderPoints = findViewById(R.id.trader_skill_input);
+        editEngineerPoints = findViewById(R.id.engineer_skill_input);
+        difficultySpinner = findViewById(R.id.difficulty_spinner);
+
+        ArrayAdapter<GameDifficulty> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,
+                Arrays.asList(GameDifficulty.values()));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        difficultySpinner.setAdapter(adapter);
+
+        Button submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "You clicked me",
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
