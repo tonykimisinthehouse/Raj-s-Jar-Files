061afb39b81f63ef27845ad86609fe37620d2054
mljustinli <ml.justin.li@gmail.com>
2019-02-17 11:32:46 -0500

    Add Tony's changes to UI and MainActivity.

--- android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/MainActivity.java
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/MainActivity.java
@@ -1,11 +1,10 @@
 package edu.gatech.cs2340.rajsjarfiles.spacetrader;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        Button submitButton = (Button) findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                       "You clicked me",
+                       Toast.LENGTH_SHORT).show();
             }
         });
     }
---
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/views/ConfigActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.cs2340.rajsjarfiles.spacetrader.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.cs2340.rajsjarfiles.spacetrader.R;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
