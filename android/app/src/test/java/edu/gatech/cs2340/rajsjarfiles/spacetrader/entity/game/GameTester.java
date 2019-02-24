089920660aba862848a73b0a831827325b9d5e92
mljustinli <ml.justin.li@gmail.com>
2019-02-24 18:57:48 -0500

    Add Javadocs to GameTester.

--- android/app/src/test/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/entity/game/GameTester.java
+++ android/app/src/test/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/entity/game/GameTester.java
@@ -4,9 +4,15 @@ import org.junit.Test;
 
 import edu.gatech.cs2340.rajsjarfiles.spacetrader.entity.universe.Universe;
 
+/**
+ * Tests the Game class.
+ */
 public class GameTester {
     public static final int TIMEOUT = 200;
 
+    /**
+     * Outputs the game to the console.
+     */
     @Test
     public void testGameToString() {
         Game game = new Game(GameDifficulty.BEGINNER);
