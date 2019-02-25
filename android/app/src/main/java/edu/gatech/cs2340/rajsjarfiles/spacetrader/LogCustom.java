42aa36be749c117d9141e22c0b9ccf53b91fd25e
mljustinli <ml.justin.li@gmail.com>
2019-02-25 13:58:01 -0500

    Add custom log method from Bob to print all of the Universe toString().

---
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/LogCustom.java
@@ -0,0 +1,24 @@
+package edu.gatech.cs2340.rajsjarfiles.spacetrader;
+
+import android.util.Log;
+
+/**
+ * Custom logging class for messages more than 4000 characters.
+ */
+public class LogCustom {
+
+    /**
+     * Custom logging method for long messages.
+     *
+     * @param tag the tag
+     * @param content the message
+     */
+    public static void largeLog(String tag, String content) {
+        if (content.length() > 4000) {
+            Log.d(tag, content.substring(0, 4000));
+            largeLog(tag, content.substring(4000));
+        } else {
+            Log.d(tag, content);
+        }
+    }
+}
