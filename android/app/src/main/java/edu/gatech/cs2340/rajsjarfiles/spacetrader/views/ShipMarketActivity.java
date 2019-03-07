15687471526e45111c42323c5bf7fcb41c3e025f
Thomas Suarez <tomthecarrot@users.noreply.github.com>
2019-03-06 22:04:29 -0500

    Remove old toolbar XML refs

--- android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/views/ShipMarketActivity.java
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/views/ShipMarketActivity.java
@@ -15,17 +15,6 @@ public class ShipMarketActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ship_market);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
