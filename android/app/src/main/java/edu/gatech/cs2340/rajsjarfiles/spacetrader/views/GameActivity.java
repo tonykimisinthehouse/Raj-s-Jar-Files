b95dd8541983985a549b3428962498319ad5fa06
tomthecarrot <code@tomthecarrot.com>
2019-03-11 16:12:47 -0400

    Add openMarket() segue

--- android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/views/GameActivity.java
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/views/GameActivity.java
@@ -1,5 +1,6 @@
 package edu.gatech.cs2340.rajsjarfiles.spacetrader.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,12 +10,20 @@ import android.view.View;
 
 import edu.gatech.cs2340.rajsjarfiles.spacetrader.R;
 
+/**
+ * Main activity while playing the game
+ */
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void openMarket(View view) {
+        Intent intent = new Intent(view.getContext(), ShipMarketActivity.class);
+        startActivity(intent);
     }
 
 }
