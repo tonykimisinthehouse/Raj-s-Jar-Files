9e93feb8dc78d010bd61385b95b20fe70046e1e1
Thomas Suarez <tomthecarrot@users.noreply.github.com>
2019-03-11 17:24:31 -0400

    Implement some ShipMarketActivity

--- android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/views/GameActivity.java
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/views/GameActivity.java
@@ -1,24 +1,29 @@
 package edu.gatech.cs2340.rajsjarfiles.spacetrader.views;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import edu.gatech.cs2340.rajsjarfiles.spacetrader.R;
+import edu.gatech.cs2340.rajsjarfiles.spacetrader.viewmodels.GameViewModel;
 
 /**
  * Main activity while playing the game
  */
 public class GameActivity extends AppCompatActivity {
 
+    /**
+     * View model for Game activity
+     */
+    private GameViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        viewModel = ViewModelProviders.of(this).get(GameViewModel.class);
     }
 
     public void openMarket(View view) {
--- android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/views/ShipMarketActivity.java
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/views/ShipMarketActivity.java
@@ -1,20 +1,84 @@
 package edu.gatech.cs2340.rajsjarfiles.spacetrader.views;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CheckedTextView;
+import android.widget.ListView;
 
-import edu.gatech.cs2340.rajsjarfiles.spacetrader.R;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import edu.gatech.cs2340.rajsjarfiles.spacetrader.entity.player.Player;
+import edu.gatech.cs2340.rajsjarfiles.spacetrader.helper.ListViewItemDTO;
+import edu.gatech.cs2340.rajsjarfiles.spacetrader.model.Model;
+import edu.gatech.cs2340.rajsjarfiles.spacetrader.viewmodels.ShipMarketViewModel;
 
 public class ShipMarketActivity extends AppCompatActivity {
+
+    /**
+     * View model for ShipMarket activity
+     */
+    private ShipMarketViewModel viewModel;
+
+    private ListView selectorGoodsForSale;
+    private ListView selectorGoodsOnShip;
+
+    private List<ListViewItemDTO> listGoodsForSale;
+    private List<ListViewItemDTO> listGoodsOnShip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ship_market);
+
+        this.viewModel = ViewModelProviders.of(this).get(ShipMarketViewModel.class);
+
+        this.assignViews();
+        this.updateGoods();
+    }
+
+    private void assignViews() {
+        this.selectorGoodsForSale = findViewById(R.id.goodsForSale);
+        this.selectorGoodsOnShip = findViewById(R.id.goodsOnShip);
+    }
+
+    private void updateGoods() {
+        // Get latest string representations of goods
+        try {
+            Player player = Model.current.getPlayer();
+            String goodsForSale = player.getPlanet().getMarketplace().toString();
+            String goodsOnShip = player.getShip().getGoods();
+
+            // Split by newlines to get arrays
+            this.listGoodsForSale = new ArrayList<>();
+            this.listGoodsOnShip = new ArrayList<>();
+            for (String desc : goodsForSale.split("\n")) {
+                ListViewItemDTO item = new ListViewItemDTO();
+                item.setItemText(desc);
+                this.listGoodsForSale.add(item);
+            }
+            for (String desc : goodsOnShip.split("\n")) {
+                ListViewItemDTO item = new ListViewItemDTO();
+                item.setItemText(desc);
+                this.listGoodsOnShip.add(item);
+            }
+        }
+        catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void buyGoods(View v) {
+        // TODO get list of checked goods
+        // TODO tell market to sell
+    }
+
+    public void sellGoods(View v) {
+        // TODO get list of checked goods
+        // TODO tell ship to sell
     }
 
 }
