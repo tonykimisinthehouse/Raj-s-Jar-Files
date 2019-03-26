c911301c0e70ebbcd4aed4acd1c4b2e1f34cbdef
mljustinli <ml.justin.li@gmail.com>
2019-03-26 15:26:47 -0400

    Add javadocs for ModelSaver.java

--- android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/model/ModelSaver.java
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/model/ModelSaver.java
@@ -9,16 +9,27 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+/**
+ * Class used to save game models.
+ */
 public class ModelSaver {
     public static final String DEFAULT_MODEL_FILE_NAME = "model.bin";
 
+    /**
+     * Saves the singleton model to a given file path. The file path is a
+     * parameter because saving requires knowing the package context.
+     *
+     * @param filePath the file path
+     * @return whether or not the model is saved successfully
+     */
     public static boolean saveBinaryModel(String filePath) {
         boolean success = true;
         try {
             Log.d("ModelSaver", filePath);
             File file = new File(filePath + DEFAULT_MODEL_FILE_NAME);
 
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
+            ObjectOutputStream out = new ObjectOutputStream(
+                    new FileOutputStream(file));
             out.writeObject(Model.current);
             out.close();
         } catch (IOException e) {
@@ -28,6 +39,13 @@ public class ModelSaver {
         return success;
     }
 
+    /**
+     * Loads the singleton model from a given file path. The file path is a
+     * parameter because saving requires knowing the package context.
+     *
+     * @param filePath the file path
+     * @return whether or not the model is loaded successfully
+     */
     public static Model loadBinaryModel(String filePath) {
         Model model = null;
 
@@ -35,14 +53,16 @@ public class ModelSaver {
             Log.d("ModelSaver", "loading from: " + filePath);
             File file = new File(filePath + DEFAULT_MODEL_FILE_NAME);
 
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+            ObjectInputStream in = new ObjectInputStream(
+                    new FileInputStream(file));
             model = (Model) in.readObject();
             Model.current = model;
             in.close();
         } catch (IOException e) {
             Log.e("ModelSaver", "Error reading an entry from binary file", e);
         } catch (ClassNotFoundException e) {
-            Log.e("ModelSaver", "Error casting a class from the binary file", e);
+            Log.e("ModelSaver",
+                    "Error casting a class from the binary file", e);
         }
         return model;
     }
