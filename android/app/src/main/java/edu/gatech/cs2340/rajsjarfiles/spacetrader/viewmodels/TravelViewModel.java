1bfae925603a3538249e584f4a15d57a2ad4517b
Thomas Suarez <code@tomthecarrot.com>
2019-03-29 12:00:43 -0400

    Implement travel UI (wip)

---
+++ android/app/src/main/java/edu/gatech/cs2340/rajsjarfiles/spacetrader/viewmodels/TravelViewModel.java
@@ -0,0 +1,33 @@
+package edu.gatech.cs2340.rajsjarfiles.spacetrader.viewmodels;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+import edu.gatech.cs2340.rajsjarfiles.spacetrader.entity.player.Ship;
+import edu.gatech.cs2340.rajsjarfiles.spacetrader.model.Model;
+
+/**
+ * Represents the view model for Travel Activity.
+ */
+public class TravelViewModel extends AndroidViewModel {
+
+    /**
+     * Backing model
+     */
+    public Model model;
+
+    /**
+     * ViewModel constructor with all arguments.
+     *
+     * @param application represents the application context
+     */
+    public TravelViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void checkDestination() {
+        // Verify the chosen destination is reachable by the ship
+    }
+
+}
